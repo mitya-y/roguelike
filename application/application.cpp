a69a08e10e2525a21a5ac8398613ab9e3b457fce
vano105 <ivankhromov05@mail.ru>
2024-10-24 02:01:47 +0300

    Add singolton of application

--- application/application.cpp
+++ application/application.cpp
@@ -1,19 +1,20 @@
 #include "application.hpp"
+#include <memory>
 
 Application::Application() {}
 Application::~Application() {}
 
-void Application::start(const Scene &Scene) {}
+void Application::start(std::unique_ptr<Scene> scene) {}
 
-Scene &Application::get_scene() { return scene; }
+Scene &Application::get_scene() { return *scene; }
 
-void Application::set_scene(const Scene &scene) {}
+void Application::set_scene(std::unique_ptr<Scene> scene) {}
 
-size_t Application::timer() {}
+double Application::timer() { return 0.0; }
 
-Model Application::create_model() {}
+std::shared_ptr<Model> Application::create_model() { return {}; }
 
-void Application::delete_model(Model *ptr) {}
+void Application::delete_model(std::shared_ptr<Model> model) {}
 
 void Application::draw_model(const Model &model, const Vec3 &scale,
                              const Vec3 &translate, const Rotation &rotate) {}
--- application/application.hpp
+++ application/application.hpp
@@ -1,24 +1,31 @@
 #pragma once
 
-#include "../model/model.hpp"
+#include "../model/model.h"
+#include <memory>
 
 class Scene {};
 
 class Application {
 private:
-  Scene scene;
+  std::unique_ptr<Scene> scene;
+  Application();
 
 public:
+  static Application &get_app() {
+    static Application app;
+
+    return app;
+  }
+
   friend class Model;
-  Application();
   ~Application();
 
-  void start(const Scene &scene);
+  void start(std::unique_ptr<Scene> scene);
   Scene &get_scene();
-  void set_scene(const Scene &scene);
-  size_t timer();
-  Model create_model();
-  void delete_model(Model *ptr);
+  void set_scene(std::unique_ptr<Scene> scene);
+  double timer();
+  std::shared_ptr<Model> create_model();
+  void delete_model(std::shared_ptr<Model> model);
   void draw_model(const Model &model, const Vec3 &scale, const Vec3 &translate,
                   const Rotation &rotate);
 };
