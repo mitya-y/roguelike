cb32efc2046e8542eea080c0dee399f397459a28
Dmitriy Kozenko <8863314@gmail.com>
2024-10-24 21:46:44 +0300

    Added glfw window for rendering

--- application/application.cpp
+++ application/application.cpp
@@ -1,10 +1,46 @@
-#include "application.hpp"
+#include <iostream>
+#include <stdexcept>
 #include <memory>
 
-Application::Application() {}
+#include <GLFW/glfw3.h>
+
+#include "application.hpp"
+
+Application::Application() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
 Application::~Application() {}
 
-void Application::start(std::unique_ptr<Scene> scene) {}
+void Application::start(std::unique_ptr<Scene> scene) {
+  // set scene
+
+  _window = glfwCreateWindow(800, 600, "roguelike", nullptr, nullptr);
+  if (_window == nullptr) {
+    throw std::runtime_error("Failed to create GLFW window");
+    glfwTerminate();
+  }
+  glfwMakeContextCurrent(_window);
+
+  while (not glfwWindowShouldClose(_window)) {
+    // render scene
+
+    glfwSwapBuffers(_window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+}
+
+bool Application::key_pressed(int key) {
+  if (_window == nullptr) {
+    return false;
+  }
+  return glfwGetKey(_window, key) == GLFW_PRESS;
+}
 
 Scene &Application::get_scene() { return *scene; }
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include "application/application.hpp"
+
 int main() {
+  Application::get_app().start(std::make_unique<Scene>());
+
   std::cout << "cgsg forever\n";
 }
